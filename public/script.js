6ba93b0fc1931e72187156b444ccd9f3a4f8a529
rudrodip <official.rudrodipsarker@gmail.com>
2022-08-27 13:12:21 +0600

    improvement

--- public/script.js
+++ public/script.js
@@ -1,13 +1,208 @@
-let side_length = 10;
-let num_of_guess = 1000;
-let num_of_iteration = 100;
+// initiating canvas and context variables
+let pi_canvas
+let graph_canvas
+let pi_ctx
+let graph_ctx
 
+// canvas scaling factor
+let pi_canvas_scale
+let graph_canvas_scale
+
+// animation variables
+let graphAnimation
+let piAnimation
+
+// speed
+let speed = 1
+
+// scaling factor logic
+if (window.innerWidth > 750) {
+    pi_canvas_scale = 0.3
+    graph_canvas_scale = 0.5
+} else {
+    pi_canvas_scale = 0.5
+    graph_canvas_scale = 0.7
+}
+
+// variables for algorithm
+let guessRange = 10
+let numberOfGuess = 10
+let iterVal = 0
+let pi = 3
 let x_prev = 0
-let y_prev = 0
+let y_prev = (window.innerWidth * graph_canvas_scale) / 2
 
-const inside_color = "red";
-const outside_color = "green";
+// window onload trigger
+window.onload = () => {
+    pi_canvas = document.getElementById('canvas')
+    graph_canvas = document.getElementById('graph')
 
+    pi_ctx = pi_canvas.getContext('2d')
+    graph_ctx = graph_canvas.getContext('2d')
+
+    pi_canvas.width = window.innerWidth * pi_canvas_scale
+    pi_canvas.height = window.innerWidth * pi_canvas_scale
+
+    graph_canvas.width = window.innerWidth * graph_canvas_scale
+    graph_canvas.height = window.innerWidth * pi_canvas_scale
+
+    let graphPlot = new GraphPlot(graph_ctx, graph_canvas.width, graph_canvas.height)
+    let piPlot = new PiPlot(pi_ctx, pi_canvas.width, pi_canvas.height)
+}
+
+// resize event listener
+window.addEventListener('resize', ()=> {
+    pi_canvas.width = window.innerWidth * pi_canvas_scale
+    pi_canvas.height = window.innerWidth * pi_canvas_scale
+
+    graph_canvas.width = window.innerWidth * graph_canvas_scale
+    graph_canvas.height = window.innerWidth * pi_canvas_scale
+})
+
+
+class GraphPlot {
+    #ctx
+    #width
+    #height
+    #pointColor = 'red'
+    #pointSize = 1
+    #lineColor = 'red'
+    #axisColor = 'aquamarine'
+
+    constructor(ctx, width, height) {
+        this.#ctx = ctx
+        this.#width = width
+        this.#height = height
+        this.#drawAxis()
+    }
+
+    #drawLine(x_prev, y_prev, x, y){
+        this.#ctx.strokeStyle = this.#lineColor
+        this.#ctx.beginPath()
+        this.#ctx.moveTo(x_prev, y_prev)
+        this.#ctx.lineTo(x, y)
+        this.#ctx.stroke()
+    }
+
+    #drawPoint(x, y){
+        this.#ctx.beginPath()
+        this.#ctx.fillStyle = this.#pointColor
+        this.#ctx.arc(x, y, this.#pointSize, 0, Math.PI * 2)
+        this.#ctx.closePath()
+        this.#ctx.fill()
+    }
+
+    #drawAxis(){
+        this.#ctx.strokeStyle = this.#axisColor
+        this.#ctx.beginPath()
+        this.#ctx.moveTo(0, this.#height/2)
+        this.#ctx.lineTo(this.#width, this.#height/2)
+        this.#ctx.stroke()
+
+        this.#ctx.font = '15px Arial'
+        this.#ctx.fillStyle = this.#axisColor
+        this.#ctx.textAlign = 'center'
+        this.#ctx.fillText(Math.PI, 70, this.#height/2 + 18)
+
+        this.#ctx.font = '20px Arial'
+        this.#ctx.fillStyle = 'magenta'
+        this.#ctx.fillText("Graph", this.#width/2, 25)
+    }
+
+    animate(){
+        let deltaPi = Math.PI - pi
+        let y_cor = deltaPi * 100 + this.#height / 2
+        let x_cor = iterVal * (this.#width / numberOfGuess)
+
+        this.#drawPoint(x_cor, y_cor)
+        this.#drawLine(x_prev, y_prev, x_cor, y_cor)
+        x_prev = x_cor
+        y_prev = y_cor
+
+        if (iterVal >= numberOfGuess) return
+
+        graphAnimation = requestAnimationFrame(this.animate.bind(this))
+    }
+}
+
+class PiPlot {
+    #ctx
+    #width
+    #height
+    #pointInsideColor = 'rgba(82,187,217,255)'
+    #pointOutsideColor = 'rgba(203, 43, 183, 1)'
+    #pointSize = 2
+    #arcColor = 'white'
+    #lineWidth = 2
+    inside = 0
+    outside = 0
+
+    constructor(ctx, width, height){
+        this.#ctx = ctx
+        this.#width = width
+        this.#height = height
+        this.#drawArc()
+    }
+
+    #drawPoint(x, y, color){
+        this.#ctx.beginPath()
+        this.#ctx.fillStyle = color
+        this.#ctx.arc(x, y, this.#pointSize, 0, Math.PI * 2)
+        this.#ctx.closePath()
+        this.#ctx.fill()
+    }
+
+    #drawArc(){
+        this.#ctx.strokeStyle = this.#arcColor
+        this.#ctx.lineWidth = this.#lineWidth
+
+        this.#ctx.beginPath()
+        this.#ctx.arc(0, this.#height, this.#width, 0, Math.PI*2)
+        this.#ctx.stroke()
+    }
+
+    #transformCoordinate(x, y){
+        let x_cartesian = (this.#width / guessRange) * x
+        let y_cartesian = this.#height - (this.#height / guessRange) * y
+        return {
+            x: x_cartesian,
+            y: y_cartesian
+        }
+    }
+
+    approx(){
+        let x_cor = Math.random() * guessRange;
+        let y_cor = Math.random() * guessRange;
+
+        let squareNum = x_cor ** 2 + y_cor ** 2
+        let coords = this.#transformCoordinate(x_cor, y_cor)
+
+        if (squareNum <= guessRange ** 2){
+            this.inside += 1
+            this.#drawPoint(coords.x, coords.y, this.#pointInsideColor)
+        } else{
+            this.#drawPoint(coords.x, coords.y, this.#pointOutsideColor)
+            this.outside += 1
+        } 
+        pi = 4 * (this.inside / (this.inside + this.outside))
+
+    }
+
+    animate(){
+        for (let i = 0; i < speed; i++){
+            if (iterVal < numberOfGuess) this.approx()
+            iterVal++
+        }
+        pi_dom.innerText = pi.toString().slice(2)
+
+        if (iterVal >= numberOfGuess){
+            return
+        }
+        piAnimation = requestAnimationFrame(this.animate.bind(this))
+    }
+}
+
+// dom element
 const iteration_dom = document.getElementById("iteration");
 const iteration_range = document.getElementById("iteration_range");
 const pi_dom = document.getElementById("decimal");
@@ -15,130 +210,28 @@ const pi_dom = document.getElementById("decimal");
 iteration_dom.innerText = iteration_range.value;
 iteration_range.addEventListener("input", (e) => {
   iteration_dom.innerText = iteration_range.value;
-  num_of_guess = parseInt(iteration_range.value);
+  numberOfGuess = parseInt(iteration_range.value);
 });
 
-const canvas = document.getElementById("canvas");
-const graph = document.getElementById("graph");
-const ctx = canvas.getContext("2d");
-const graph_ctx = graph.getContext("2d");
+function runSimulation(){
+    cancelAnimationFrame(piAnimation)
+    cancelAnimationFrame(graphAnimation)
 
-const width = 500;
-const graph_width = 800;
+    pi_ctx.clearRect(0, 0, pi_canvas.width, pi_canvas.height)
+    graph_ctx.clearRect(0, 0, graph_canvas.width, graph_canvas.height)
 
-canvas.clientWidth = width;
-canvas.clientHeight = width;
-graph.clientWidth = graph_width;
-graph.clientHeight = width;
+    iterVal = 0
+    x_prev = 0
+    y_prev = 0
 
-window.addEventListener("resize", () => {
-  canvas.clientWidth = width;
-  canvas.clientHeight = width;
-  graph.clientWidth = graph_width;
-  graph.clientHeight = width;
-});
+    let graphPlot = new GraphPlot(graph_ctx, graph_canvas.width, graph_canvas.height)
+    let piPlot = new PiPlot(pi_ctx, pi_canvas.width, pi_canvas.height)
 
-ctx.strokeStyle = "white";
-ctx.lineWidth = 2;
-
-graph_ctx.strokeStyle = "white";
-graph_ctx.lineWidth = 2;
-
-ctx.beginPath();
-ctx.arc(0, width, width, 0, Math.PI * 2);
-ctx.stroke();
-
-function approx(side_length, number_of_guess) {
-  let inside = 0;
-  let outside = 0;
-
-  for (let i = 0; i < number_of_guess; i++) {
-    x_cor = Math.random() * side_length;
-    y_cor = Math.random() * side_length;
-
-    square_sum = x_cor ** 2 + y_cor ** 2;
-    let coords = transformCoordinate(x_cor, y_cor, side_length);
-
-    if (square_sum <= side_length ** 2) {
-      inside += 1;
-      drawPoint(coords[0], coords[1], inside_color, ctx);
-    } else {
-      outside += 1;
-      drawPoint(coords[0], coords[1], outside_color, ctx);
-    }
-  }
-
-  let pi = 4 * (inside / (inside + outside));
-  return pi;
+    piPlot.animate(0)
+    graphPlot.animate(0)
 }
 
-function transformCoordinate(x, y, side_length) {
-  x_cartesian = (width / side_length) * x;
-  y_cartesian = width - (width / side_length) * y;
-  return [x_cartesian, y_cartesian];
-}
-
-function drawPoint(x, y, color, context, size = 2) {
-  context.fillStyle = color;
-  context.fillRect(x, y, size, size);
-}
-
-function runSimulationOnce(n) {
-  ctx.clearRect(0, 0, width, width);
-
-  let pi = approx(side_length, num_of_guess);
-  pi_dom.innerText = pi.toString().slice(2);
-
-  drawGraphPoints(pi, n)
-
-  ctx.beginPath();
-  ctx.arc(0, width, width, 0, Math.PI * 2);
-  ctx.stroke();
-}
-
-function drawCoordinate() {
-  graph_ctx.beginPath();
-  graph_ctx.moveTo(0, width/2);
-  graph_ctx.lineTo(graph_width, width/2);
-  graph_ctx.stroke();
-
-  graph_ctx.font = "15px Arial"
-  graph_ctx.fillStyle = "aquamarine"
-  graph_ctx.textAlign = "center"
-  graph_ctx.fillText(Math.PI, 70, width/2 + 18)
-
-  graph_ctx.font = "20px Arial"
-  graph_ctx.fillStyle = "white"
-  graph_ctx.fillText("Graph", graph_width/2, 25)
-
-}
-
-drawCoordinate();
-
-function drawGraphPoints(val, n) {
-  let delta = Math.PI - val
-  y_cor = delta * 1000 + width / 2
-  x_cor = n * (graph_width/num_of_iteration)
-
-  graph_ctx.strokeStyle = "deepskyblue"
-  graph_ctx.lineWidth = 1
-  graph_ctx.beginPath()
-  graph_ctx.moveTo(x_prev, y_prev)
-  graph_ctx.lineTo(x_cor, y_cor)
-  graph_ctx.stroke()
-
-  drawPoint(x_cor, y_cor, "green", graph_ctx, 0)
-  x_prev = x_cor
-  y_prev = y_cor
-}
-
-function runSimulation() {
-  graph_ctx.clearRect(0, 0, graph_width, width)
-  x_prev = 0
-  y_prev = 0
-  drawCoordinate()
-
-  for (let i = 0; i < num_of_iteration; i++){
-    runSimulationOnce(i)
-  }
+function stopSimulation(){
+    cancelAnimationFrame(piAnimation)
+    cancelAnimationFrame(graphAnimation)
 }
